0e3af140a93f48829f192612fa4e7c6f175b96e6
Miklos Nemeth <nemethmiklos1960@gmail.com>
2019-08-05 13:42:46 +0200

    Initiated with Dart stagehand package-simple

---
+++ example/b1serviceflayer_example.dart
@@ -0,0 +1,6 @@
+import 'package:b1serviceflayer/b1serviceflayer.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/b1serviceflayer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library b1serviceflayer;
+
+export 'src/b1serviceflayer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
